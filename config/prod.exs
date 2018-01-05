03a9e72a12014614566515524ccbcced95c9a408
Boaz Blake <boazblake@gmail.com>
2018-01-04 21:19:29 -0600

    toDeploy

--- config/prod.exs
+++ config/prod.exs
@@ -15,12 +15,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :landing_page, LandingPageWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme:"https", host: "boaz-landing-page.herokuapp.com/", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "secret_key_base")
 
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :landingPage, landingPage.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true,
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
